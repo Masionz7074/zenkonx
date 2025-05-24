7b1736dc94123a7e53cf99595f14230640c2e907
Masionz7074 <kids.larmore22@gmail.com>
2025-05-23 23:54:39 -0400

    Update particles-config.js

--- particles-config.js
+++ particles-config.js
@@ -5,33 +5,33 @@ tsParticles.load("tsparticles", {
     },
     particles: {
         number: {
-            value: 80, // Number of particles
+            value: 60, // Reduced slightly for less clutter
             density: {
                 enable: true,
                 value_area: 800
             }
         },
         color: {
-            value: "#888888" // Color of particles
+            value: "#777777" // Particle color
         },
         shape: {
             type: "circle"
         },
         opacity: {
-            value: 0.5,
+            value: 0.4,
             random: true,
             anim: {
                 enable: true,
-                speed: 1,
+                speed: 0.8,
                 opacity_min: 0.1,
                 sync: false
             }
         },
         size: {
-            value: 3,
+            value: 2.5,
             random: true,
             anim: {
-                enable: false, // Set to true for pulsing particles
+                enable: false,
                 speed: 40,
                 size_min: 0.1,
                 sync: false
@@ -39,16 +39,16 @@ tsParticles.load("tsparticles", {
         },
         links: {
             enable: true,
-            distance: 150,
-            color: "#666666", // Color of lines connecting particles
-            opacity: 0.4,
+            distance: 160,
+            color: "#555555", // Link color
+            opacity: 0.3,
             width: 1
         },
         move: {
             enable: true,
-            speed: 2, // Speed of particle movement
+            speed: 1.5,
             direction: "none",
-            random: false,
+            random: true,
             straight: false,
             out_mode: "out",
             bounce: false,
@@ -64,34 +64,34 @@ tsParticles.load("tsparticles", {
         events: {
             onhover: {
                 enable: true,
-                mode: "grab" // "grab", "repulse", "bubble"
+                mode: "grab"
             },
             onclick: {
                 enable: true,
-                mode: "push" // "push", "remove", "bubble"
+                mode: "push"
             },
             resize: true
         },
         modes: {
             grab: {
-                distance: 140,
+                distance: 130,
                 line_linked: {
-                    opacity: 1
+                    opacity: 0.8
                 }
             },
             bubble: {
-                distance: 400,
-                size: 40,
+                distance: 350,
+                size: 30,
                 duration: 2,
                 opacity: 8,
                 speed: 3
             },
             repulse: {
-                distance: 200,
+                distance: 180,
                 duration: 0.4
             },
             push: {
-                particles_nb: 4 // Number of particles to add on click
+                particles_nb: 3
             },
             remove: {
                 particles_nb: 2
